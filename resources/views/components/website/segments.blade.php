903df3a8d89c06f0e2b3b49a473a472ad35e8b51
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-06-28 17:24:08 -0700

    Modificacion en fotos de segmentos

--- resources/views/components/website/segments.blade.php
+++ resources/views/components/website/segments.blade.php
@@ -1,5 +1,5 @@
 <section class="bg-white pb-10 md:pb-16">
-    <div class="container flex flex-col md:flex-row space-y-5 md:space-y-0 items-stretch flex-wrap justify-center">
+    <div class="container flex flex-col md:flex-row items-stretch flex-wrap justify-center">
         <div class="w-full md:w-1/4 px-2 mb-6">
             <div class="bg-white shadow rounded overflow-hidden h-full">
                 <img src="{{ asset('img/segments/commercial.jpg') }}" alt="" class="w-full h-auto object-cover">

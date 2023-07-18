d3b34e5f865a3d4cdf46e399787019a92cc969cd
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-07-18 09:21:17 -0700

    Meta Tags

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Baja California, a state with global talent in the aerospace sector, expertise in aeronautics and space, accredited SMEs supply chain value and over five decades seniority in word-class management." />
+        <meta name="keywords" content="Baja California, Mexico, Aerospace, Cluster, Tijuana, Ensenada, Mexicali, Tecate, Rosarito, Baja Aerospace Cluster, Baja California Aerospace Cluster, Baja Aerospace, Baja California Aerospace, Aerospace Cluster, Aerospace Cluster Mexico, Aerospace Cluster Baja California, Aerospace Cluster Baja, Aerospace Cluster Tijuana, Aerospace Cluster Ensenada, Aerospace Cluster Mexicali, Aerospace Cluster Tecate, Aerospace Cluster Rosarito, Aerospace Tijuana, Aerospace Ensenada, Aerospace Mexicali, Aerospace Tecate, Aerospace Rosarito, Aerospace Baja California, Aerospace Baja, Aerospace Baja Mexico, Aerospace Baja Tijuana, Aerospace Baja Ensenada, Aerospace Baja Mexicali, Aerospace Baja Tecate, Aerospace Baja Rosarito" />
+        <meta name="author" content="Baja Aerospace Cluster" />
 
         <title> @if($title) {{$title}} -  @endif {{ config('app.name', 'Laravel') }}</title>
 

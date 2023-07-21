f0308fa2bca5d4f95ab077ea248c8d759b10355f
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-07-21 08:51:47 -0700

    Page Header - Fixed

--- resources/views/components/website/page-cover.blade.php
+++ resources/views/components/website/page-cover.blade.php
@@ -1,12 +1,12 @@
 <div class="container @if($title || $subtitle) pt-16 @endif">
     @if($title)
     <h1 class="text-white text-4xl font-bold text-center uppercase">
-        {{ $title }}
+        {!! $title !!}
     </h1>
     @endif
     @if($subtitle)
     <h4 class="text-center text-white text-xl font-bold">
-        {{ $subtitle }}
+        {!! $subtitle !!}
     </h4>
     @endif
 </div>

b3ddc21454052056f5fdf0d2d8992623a1fc1b94
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-07-21 08:57:03 -0700

    Page Header - Fixed

--- resources/views/components/website/page-cover.blade.php
+++ resources/views/components/website/page-cover.blade.php
@@ -1,12 +1,12 @@
 <div class="container @if($title || $subtitle) pt-16 @endif">
     @if($title)
     <h1 class="text-white text-4xl font-bold text-center uppercase">
-        {!! $title !!}
+        {{ html_entity_decode($title) }}
     </h1>
     @endif
     @if($subtitle)
     <h4 class="text-center text-white text-xl font-bold">
-        {!! $subtitle !!}
+        {{ html_entity_decode($subtitle) }}
     </h4>
     @endif
 </div>

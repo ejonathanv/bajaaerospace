f6ef1f145755786e3fbbd75950de6eb61fce8714
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-07-28 12:32:03 -0700

    Logos partners

--- resources/views/components/website/supporting-organizations.blade.php
+++ resources/views/components/website/supporting-organizations.blade.php
@@ -5,22 +5,91 @@
         </h2>
         <div class="flex items-center flex-wrap justify-center">
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/agencia_espacial_mexicana.jpg') }}" alt="Agencia Espacial Mexicana" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/acopaer.jpg') }}" alt="Acompaer" class="w-full h-auto">
             </div>
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/agencia_federal_de_aviacion_civil.jpg') }}" alt="Agencia Federal de Aviación Civil" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/aem.jpg') }}" alt="AEM" class="w-full h-auto">
             </div>
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/ayuntamiento_tijuana.jpg') }}" alt="Ayuntamiento de Tijuana" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/afac.jpg') }}" alt="AFAC" class="w-full h-auto">
             </div>
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/baja_california_gobierno_del_estado.jpg') }}" alt="Gobierno del Estado de Baja California" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/agentes-aduanales-de-tijuana-y-tecate.jpg') }}" alt="Agentes Aduanales de Tijuana y Tecate" class="w-full h-auto">
             </div>
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/consejo_coordinador_empresarial_tijuana.jpg') }}" alt="Consejo Coordinador Empresarial de Tijuana" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/aimo.jpg') }}" alt="AIMO" class="w-full h-auto">
             </div>
             <div class="w-4/12 md:w-2/12 px-2">
-                <img src="{{ asset('img/partners/consejo_de_desarrollo_de_tijuana.jpg') }}" alt="Consejo de Desarrollo de Tijuana" class="w-full h-auto">
+                <img src="{{ asset('img/partners/logos/baja-aerospace-cluster-comision-de-mujeres.jpg') }}" alt="Baja Aerospace Cluster de Mujeres" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/baja-california-gobierno-del-estado.jpg') }}" alt="Baja California Gobierno del Estado" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/bancomext.jpg') }}" alt="Bancomext" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/caescol.jpg') }}" alt="CAESCOL" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/canacar.jpg') }}" alt="CANACAR" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/cas.jpg') }}" alt="CAS" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/cluster-aeroespacial-mendoza-argentina.jpg') }}" alt="Cluster Aeroespacial Mendoza Argentina" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/cluster-energetico-de-baja-california.jpg') }}" alt="Cluster Energético de Baja California" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/conocer.jpg') }}" alt="CONOCER" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/consejo-cordinador-empresarial-tijuana.jpg') }}" alt="Consejo Cordinador Empresarial Tijuana" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/consejo-de-desarrollo-de-tijuana.jpg') }}" alt="Consejo de Desarrollo de Tijuana" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/consejo-nacional-de-clusteres-energeticos.jpg') }}" alt="Consejo Nacional de Clústeres Energéticos" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/csipi.jpg') }}" alt="CSIPi" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/economia.jpg') }}" alt="Economía" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/ensenada.jpg') }}" alt="Ensenada" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/index.jpg') }}" alt="Index" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/mexico-business.jpg') }}" alt="Mexico Business" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/nacional-financiera.jpg') }}" alt="Nacional Financiera" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/sct.jpg') }}" alt="SCT" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/secretaria-de-economia.jpg') }}" alt="Secretaría de Economía" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/sedena.jpg') }}" alt="SEDENA" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/sedeti.jpg') }}" alt="SEDETI" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/tijuana-edc.jpg') }}" alt="Tijuana EDC" class="w-full h-auto">
+            </div>
+            <div class="w-4/12 md:w-2/12 px-2">
+                <img src="{{ asset('img/partners/logos/xxiv-ayuntamiento-tijuana.jpg') }}" alt="XXIV Ayuntamiento Tijuana" class="w-full h-auto">
             </div>
         </div>
     </div>

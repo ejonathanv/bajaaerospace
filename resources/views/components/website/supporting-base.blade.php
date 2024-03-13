636874d998cabe01b5371f3246106ee22f127814
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-03-13 15:38:11 -0700

    Actualizacion de logotipos

--- resources/views/components/website/supporting-base.blade.php
+++ resources/views/components/website/supporting-base.blade.php
@@ -4,24 +4,6 @@
             Our Supporting Base
         </h2>
         <div class="flex items-center flex-wrap justify-center">
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/aem.jpg') }}" alt="AEM" class="w-full h-auto">
-            </div>
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/afac.jpg') }}" alt="AFAC" class="w-full h-auto">
-            </div>
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/xxiv-ayuntamiento-tijuana.jpg') }}" alt="XXIV Ayuntamiento Tijuana" class="w-full h-auto">
-            </div>
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/baja-california-gobierno-del-estado.jpg') }}" alt="Baja California Gobierno del Estado" class="w-full h-auto">
-            </div>
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/consejo-cordinador-empresarial-tijuana.jpg') }}" alt="Consejo Cordinador Empresarial Tijuana" class="w-full h-auto">
-            </div>
-            <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                <img src="{{ asset('img/partners/logos/consejo-de-desarrollo-de-tijuana.jpg') }}" alt="Consejo de Desarrollo de Tijuana" class="w-full h-auto">
-            </div>
             @for($i = 1; $i <= 102; $i++)
                 @php
                     $number = str_pad($i, 5, '0', STR_PAD_LEFT);

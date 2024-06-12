9e810cb515324e6024ba294d824eb3702de81874
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-06-12 16:57:53 -0700

    Ruta de logos de miembros

--- resources/views/website/members.blade.php
+++ resources/views/website/members.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="flex items-center flex-wrap justify-center">
                 @for($i = 1; $i <= 78; $i++) @php $number=str_pad($i, 5, '0' , STR_PAD_LEFT); @endphp <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                    <img src="{{ asset('img/partners/logos2024/actualizados/logo'.$number.'.jpg') }}" class="w-full h-auto">
+                    <img src="{{ asset('img/members/logo'.$number.'.jpg') }}" class="w-full h-auto">
             </div>
             @endfor
         </div>

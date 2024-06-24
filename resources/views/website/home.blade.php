e5c36f96ced19b312b013c05abfd55cb6c71189d
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-06-24 12:52:33 -0700

    Enlace de banner de dell

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -5,7 +5,9 @@
     <section class="bg-gray-100 pb-10 md:pb-20">
         <div class="container">
             <div class="w-full md:w-9/12 mx-auto">
+                <a href="https://decsef.com/" target="_blank" rel="noopener noreferrer">
                 <img src="{{ asset('img/banners/dell_baja_aerospace_cluster.png') }}" alt="Dell Baja Aerospace Cluster" class="w-full h-auto shadow rounded">
+                </a>
             </div>
         </div>
     </section>

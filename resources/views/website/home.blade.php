f895a890a0cb65f5a58db8a43d79ec30879e5608
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-06-28 12:07:57 -0700

    Actualizacion de banner

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="w-full md:w-9/12 mx-auto">
                 <a href="https://decsef.com/" target="_blank" rel="noopener noreferrer">
-                <img src="{{ asset('img/banners/dell_baja_aerospace_cluster.png') }}" alt="Dell Baja Aerospace Cluster" class="w-full h-auto shadow rounded">
+                <img src="{{ asset('img/banners/dell_baja_aerospace_cluster.JPG') }}" alt="Dell Baja Aerospace Cluster" class="w-full h-auto shadow rounded">
                 </a>
             </div>
         </div>

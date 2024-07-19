7240b07c5717c5cb0a9746a632b5da61dc0c69a9
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-07-19 10:47:46 -0700

    Members logos - updated

--- resources/views/website/members.blade.php
+++ resources/views/website/members.blade.php
@@ -4,7 +4,7 @@
     <section class="flex flex-col md:flex-row items-center md:items-stretch bg-white py-16">
         <div class="container">
             <div class="flex items-center flex-wrap justify-center">
-                @for($i = 1; $i <= 78; $i++) @php $number=str_pad($i, 5, '0' , STR_PAD_LEFT); @endphp <div class="w-4/12 md:w-1/12 px-2 mb-4">
+                @for($i = 1; $i <= 84; $i++) @php $number=str_pad($i, 5, '0' , STR_PAD_LEFT); @endphp <div class="w-4/12 md:w-1/12 px-2 mb-4">
                     <img src="{{ asset('img/members/logo'.$number.'.jpg') }}" class="w-full h-auto">
             </div>
             @endfor

3bb4299daa06a94029d11a7abab1a29223557656
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2024-12-23 13:31:44 -0800

    Se acomoda el grid de video blog

--- resources/views/website/video-blog.blade.php
+++ resources/views/website/video-blog.blade.php
@@ -2,9 +2,9 @@
     <section class="py-16 bg-gray-100">
         @if($videos->count())
         <div class="container-fluid">
-            <div class="flex flex-col md:flex-wrap items-stretch -mx-3">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 -mx-3">
                 @foreach($videos as $video)
-                <div class="w-full md:w-1/3 p-3 mb-6">
+                <div class="p-3 mb-6">
                     <x-video-card :video="$video" theme="light"></x-video-card>
                 </div>
                 @endforeach

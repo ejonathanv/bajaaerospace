6e6ad5090e87020d8f51a011d1f6b1924efdf4f2
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2025-06-27 12:45:22 -0700

    Se ajusta el tamaño de los logos de miembros y se mantiene el número de 113 logos

--- resources/views/website/members.blade.php
+++ resources/views/website/members.blade.php
@@ -4,9 +4,19 @@
     <section class="flex flex-col md:flex-row items-center md:items-stretch bg-white py-16">
         <div class="container">
             <div class="flex items-center flex-wrap justify-center">
-                @for($i = 1; $i <= 113; $i++) @php $number=str_pad($i, 5, '0' , STR_PAD_LEFT); @endphp <div class="w-4/12 md:w-1/12 px-2 mb-4">
-                    <img src="{{ asset('img/members/logo'.$number.'.jpg') }}" class="w-full h-auto">
-            </div>
+            @for($i = 1; $i <= 113; $i++) 
+                @php $number=str_pad($i, 5, '0' , STR_PAD_LEFT); @endphp 
+                <div class="px-2 mb-4">
+                    @if($i == 111)
+                        <div class="w-full h-auto">
+                            <img src="{{ asset('img/members/logo'.$number.'.jpg') }}" style="width: 125px; height: auto;">
+                        </div>
+                    @else
+                        <div class="w-full h-auto">
+                            <img src="{{ asset('img/members/logo'.$number.'.jpg') }}" style="width: 75px; height: auto;">
+                        </div>
+                    @endif
+                </div>
             @endfor
         </div>
         </div>
